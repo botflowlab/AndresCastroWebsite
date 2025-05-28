b190321773cb3fe6aacfe330a46c491f61588ebf
botflowlab <botflowlab@gmail.com>
2025-05-28 11:17:45 -0600

    Create basic website with Vite and React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="text-4xl font-bold text-gray-800">
+        Welcome to my website!
+      </div>
+    </div>
+  )
+}
+
+export default App
