a93506c77c614844e8591338a4c768bb6089272a
botflowlab <botflowlab@gmail.com>
2025-05-29 16:12:08 -0600

    Update translation files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,6 @@ function Footer() {
         <div>
           <h3 className="text-xl mb-6">{t('footer.help')}</h3>
           <nav className="space-y-4">
-            <a href="/about" className="block hover:text-gray-300">{t('footer.about')}</a>
             <a href="/projects" className="block hover:text-gray-300">{t('footer.featuredProjects')}</a>
             <a href="/architect" className="block hover:text-gray-300">{t('footer.architect')}</a>
             <a href="/publications" className="block hover:text-gray-300">{t('footer.publications')}</a>
