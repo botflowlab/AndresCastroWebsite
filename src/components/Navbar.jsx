d12cac6e7a4c49b50fb25f44b7bcc595023b4d70
botflowlab <botflowlab@gmail.com>
2025-05-29 15:43:37 -0600

    Create Navigation Bar Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,10 +19,8 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           {/* Logo placeholder */}
-          <div className="flex-shrink-0">
-            <div className="h-12 w-32 bg-transparent">
-              <img src="/images/andrescastrologohor.png" alt="Logo" className="h-full object-contain" />
-            </div>
+          <div className="flex-shrink-0 flex items-center">
+            <img src="/images/andrescastrologohor.png" alt="Logo" className="h-16 md:h-20 object-contain" />
           </div>
 
           {/* Navigation Links */}
