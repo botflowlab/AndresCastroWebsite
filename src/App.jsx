f4153ecd24553f4fb2426e7e8dc3185ad5caea99
botflowlab <botflowlab@gmail.com>
2025-05-30 16:30:44 -0600

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/projects/Projects';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/proyectos" element={<Projects />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/proyectos" element={<Projects />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
