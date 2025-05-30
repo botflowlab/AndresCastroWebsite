b17721df1817824d92151640e38c8534de5e4d05
botflowlab <botflowlab@gmail.com>
2025-05-29 23:30:00 -0600

    Updated ProjectSidebar.jsx

--- src/pages/projects/ProjectSidebar.jsx
+++ src/pages/projects/ProjectSidebar.jsx
@@ -6,7 +6,7 @@ export default function Sidebar({ onItemClick }) {
   ];
 
   return (
-    <aside className="hidden md:flex flex-col gap-4 text-lg text-gray-600 w-full pl-4 pr-8">
+    <aside className="hidden md:flex flex-col gap-4 text-lg text-gray-600 w-full pl-4 pr-8 pt-16">
       <h2 className="font-bold text-5xl text-black mb-8">WORK</h2>
       {categories.map((cat) => (
         <button 
