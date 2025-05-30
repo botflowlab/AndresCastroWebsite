61b7e5c7c96deb4aea54a8bfd2afa098b94e668e
botflowlab <botflowlab@gmail.com>
2025-05-29 22:19:44 -0600

    Add missing useState import

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -8,24 +8,29 @@ export default function Projects() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="font-sans relative">
+    <div className="font-sans">
       <Navbar />
 
-      {/* Left-side Sidebar (absolute) */}
-      <div className="hidden md:block fixed left-0 top-0 h-screen w-[16%] -z-10 pt-24 px-4 bg-white">
-        <Sidebar />
-      </div>
-
-      {/* Right-side Vertical Text */}
-      <div className="hidden md:flex fixed right-0 top-0 h-screen w-[16%] items-start justify-center z-0 pointer-events-none pt-24">
-        <div className="-rotate-90 origin-top-right text-[160px] font-bold whitespace-nowrap text-transparent" style={{ WebkitTextStroke: '1px black' }}>
-          PROJECTS
+      <div className="flex pt-24 relative">
+        {/* Left-side Sidebar (static) */}
+        <div className="hidden md:block md:w-[16%] pt-4 px-4 bg-white">
+          <Sidebar />
         </div>
-      </div>
 
-      {/* Main Content Layout */}
-      <div className="pt-24 relative z-10 max-w-screen-xl mx-auto px-4 md:pl-[18%] md:pr-[18%]">
-        <ProjectGrid />
+        {/* Main Content */}
+        <div className="flex-1 md:px-12 relative z-10">
+          <ProjectGrid />
+        </div>
+
+        {/* Right-side Vertical Text */}
+        <div className="hidden md:flex md:w-[16%] items-start justify-center pt-24 pointer-events-none z-0">
+          <div
+            className="-rotate-90 origin-top-right text-[160px] font-bold whitespace-nowrap text-transparent"
+            style={{ WebkitTextStroke: '1px black' }}
+          >
+            PROJECTS
+          </div>
+        </div>
       </div>
 
       {/* Mobile Sidebar */}
