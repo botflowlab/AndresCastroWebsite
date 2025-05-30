2003ede18df08c7fafd2a8c2b9ce678fef5f2218
botflowlab <botflowlab@gmail.com>
2025-05-29 22:55:24 -0600

    Updated ProjectCard.jsx

--- src/pages/projects/ProjectCard.jsx
+++ src/pages/projects/ProjectCard.jsx
@@ -4,7 +4,7 @@ export default function ProjectCard({ title, image }) {
       <div className="aspect-[6/8] overflow-hidden">
         <img src={image} alt={title} className="w-full h-full object-cover transition-transform duration-300 hover:scale-105" />
       </div>
-      <div className="mt-2 text-center text-[0c0c0c] font-bold bg-white py-2 text-lg text-bold uppercase">{title}</div>
+      <div className="mt-2 text-center text-[0c0c0c] font-bold bg-white py-2 text-xl text-bold uppercase">{title}</div>
     </div>
   );
 }
