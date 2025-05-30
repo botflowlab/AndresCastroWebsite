94ae7ef216980f69ec41afb922e4bac90260dc7f
botflowlab <botflowlab@gmail.com>
2025-05-30 16:31:02 -0600

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/projects/Projects';
-import Layout from '/components/Layout';
+import Layout from '/src/components/Layout';
 
 function App() {
   return (
