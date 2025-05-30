7fdf7d2cb4a1a06b12029766aa0f42b5978c1606
botflowlab <botflowlab@gmail.com>
2025-05-29 22:43:26 -0600

    Updated Projects.jsx

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -24,7 +24,7 @@ export default function Projects() {
       
         {/* Main Content */}
         <div className="md:ml-64">
-          <div className="max-w-4xl mx-auto px-6 pt-6">
+          <div className="max-w-4xl mx-auto px-6 pt-16">
             <main>
               <ProjectGrid />
             </main>
