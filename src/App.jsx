fca8d034939307529d9ebac4ad34a9716c1b8608
botflowlab <botflowlab@gmail.com>
2025-05-29 20:38:23 -0600

    Organize project files structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
-import MciParking from './pages/projects/MciParking';
-import KansasMuseum from './pages/projects/KansasMuseum';
 
 function App() {
   return (
@@ -11,8 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/proyectos" element={<Projects />} />
-        <Route path="/proyectos/mci-parking" element={<MciParking />} />
-        <Route path="/proyectos/kansas-museum" element={<KansasMuseum />} />
       </Routes>
     </Router>
   );
--- src/components/sections/ProjectGrid.jsx
+++ src/components/sections/ProjectGrid.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function ProjectGrid() {
   const projects = [
@@ -7,20 +6,18 @@ function ProjectGrid() {
       id: 1,
       title: 'MCI PARKING STRUCTURE',
       image: 'https://images.pexels.com/photos/127028/pexels-photo-127028.jpeg',
-      path: '/proyectos/mci-parking'
     },
     {
       id: 2,
       title: 'KANSAS CITY MUSEUM',
       image: 'https://images.pexels.com/photos/461902/pexels-photo-461902.jpeg',
-      path: '/proyectos/kansas-museum'
     },
   ];
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
       {projects.map((project) => (
-        <Link to={project.path} key={project.id} className="relative group">
+        <div key={project.id} className="relative group">
           <div className="aspect-w-3 aspect-h-4 overflow-hidden">
             <img
               src={project.image}
@@ -33,7 +30,7 @@ function ProjectGrid() {
               {project.title}
             </h3>
           </div>
-        </Link>
+        </div>
       ))}
     </div>
   );
