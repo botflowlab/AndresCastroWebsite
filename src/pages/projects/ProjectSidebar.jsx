fdb4f3ca723b5f63dd46fa2a9396a5b15e2ff858
botflowlab <botflowlab@gmail.com>
2025-05-30 16:13:54 -0600

    Updated ProjectSidebar.jsx

--- src/pages/projects/ProjectSidebar.jsx
+++ src/pages/projects/ProjectSidebar.jsx
@@ -4,7 +4,6 @@ export default function Sidebar({ onItemClick }) {
   const { t } = useTranslation();
   const categories = [
     'sustainable',
-    'office',
     'outdoor',,,
     'infrastructure',
     'recreational',
