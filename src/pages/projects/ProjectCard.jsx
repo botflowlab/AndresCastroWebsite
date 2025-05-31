0e2a5e4c0e9de91f6edec66f0338e3889f8b7b08
botflowlab <botflowlab@gmail.com>
2025-05-31 14:09:56 -0600

    Update project cards with window-style design

--- src/pages/projects/ProjectCard.jsx
+++ src/pages/projects/ProjectCard.jsx
@@ -1,10 +1,23 @@
+import React from 'react';
+
 export default function ProjectCard({ title, image }) {
   return (
     <div className="w-full overflow-hidden">
-      <div className="aspect-[7/9] overflow-hidden">
-        <img src={image} alt={title} className="w-full h-full object-cover transition-transform duration-300 hover:scale-125" />
+      <div className="relative">
+        {/* Window frame */}
+        <div className="aspect-[7/9] overflow-hidden rounded-t-[40px] border-4 border-[#0c0c0c]">
+          <img 
+            src={image} 
+            alt={title} 
+            className="w-full h-full object-cover transition-transform duration-300 hover:scale-125" 
+          />
+        </div>
+        {/* Window sill */}
+        <div className="h-4 bg-[#0c0c0c] -mt-1"></div>
       </div>
-      <div className="mt-2 text-center text-[0c0c0c] font-bold bg-blur py-2 text-xl text-bold uppercase">{title}</div>
+      <div className="mt-4 text-center text-[#0c0c0c] font-bold py-2 text-xl uppercase">
+        {title}
+      </div>
     </div>
   );
 }
