2127b86a7f90e5f543cd725017e922572e1c2971
botflowlab <botflowlab@gmail.com>
2025-05-31 17:51:43 -0600

    Updated NewsSection.jsx

--- src/components/sections/NewsSection.jsx
+++ src/components/sections/NewsSection.jsx
@@ -35,8 +35,7 @@ function NewsSection() {
           className="absolute inset-0 w-full h-full opacity-10"
           style={{
             backgroundImage: 'url(/images/concrete2.svg)',
-            backgroundRepeat: 'repeat',
-            backgroundSize: '500px'
+            backgroundRepeat: 'repeat'
           }}
         />
         {/* Top Fade */}
