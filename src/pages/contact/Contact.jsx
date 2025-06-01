0aa5255cedecf1fcd2c05017fe9a44d7dd8e2470
botflowlab <botflowlab@gmail.com>
2025-06-01 15:46:23 -0600

    Updated Contact.jsx

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -4,7 +4,7 @@ import AgentInfo from './AgentInfo';
 
 export default function Contact() {
   return (
-    <div className="min-h-screen pt-20">
+    <div className="min-h-screen pt-20 mb-20">
       <div className="container mx-auto">
         <div className="flex flex-col md:flex-row min-h-[calc(100vh-5rem)] mt-20 items-center">
           {/* Left side - Contact Form */}
