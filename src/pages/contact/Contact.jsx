137fa25b7ead45cb689a851f2f3a6b11448621af
botflowlab <botflowlab@gmail.com>
2025-06-01 15:55:40 -0600

    Updated Contact.jsx

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -5,18 +5,6 @@ import AgentInfo from './AgentInfo';
 export default function Contact() {
   return (
     <div className="min-h-screen pt-20 relative">
-      {/* Background Image with Overlay */}
-      <div 
-        className="absolute inset-0 z-0"
-        style={{
-          backgroundImage: 'url(/images/acContact.jpg)',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center'
-        }}
-      >
-        <div className="absolute inset-0 bg-black/60"></div>
-      </div>
-
       <div className="container mx-auto relative z-10">
         <div className="flex flex-col md:flex-row min-h-[calc(100vh-5rem)] mt-20 items-center">
           {/* Left side - Contact Form */}
