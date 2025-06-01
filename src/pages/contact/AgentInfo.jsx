45a7124f57f4362a8ff7297d7a5176ba2c6e0026
botflowlab <botflowlab@gmail.com>
2025-06-01 15:44:16 -0600

    Updated AgentInfo.jsx

--- src/pages/contact/AgentInfo.jsx
+++ src/pages/contact/AgentInfo.jsx
@@ -5,7 +5,7 @@ import { MdEmail, MdPhone, MdLocationOn } from 'react-icons/md';
 export default function AgentInfo() {
   return (
     <div className="h-full bg-white flex items-center justify-center p-8 md:p-16">
-      <div className="w-full max-w-md flex flex-col items-center text-center justify-center h-full">
+      <div className="w-full max-w-md flex flex-col items-center text-center justify-center min-h-full">
         {/* Logo */}
         <img 
           src="/images/andrescastrologosolo.png" 
