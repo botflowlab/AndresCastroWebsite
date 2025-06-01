f52f26e82338e834cf85e64860517e109cbe9314
botflowlab <botflowlab@gmail.com>
2025-05-31 18:25:45 -0600

    Updated NewsSection.jsx

--- src/components/sections/NewsSection.jsx
+++ src/components/sections/NewsSection.jsx
@@ -32,7 +32,7 @@ function NewsSection() {
       <div className="absolute inset-0 w-full h-full">
         {/* Pattern Background */}
         <div 
-          className="absolute inset-0 w-full h-full opacity-20"
+          className="absolute inset-0 w-full h-full opacity-10"
           style={{
             backgroundImage: 'url(/images/concrete1.png)',
             backgroundRepeat: 'repeat',
