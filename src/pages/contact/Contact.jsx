695bf590ce1c72167c9b50fffa78e1f5ce121748
botflowlab <botflowlab@gmail.com>
2025-06-01 15:57:40 -0600

    Updated Contact.jsx

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -6,7 +6,7 @@ export default function Contact() {
   return (
     <div className="min-h-screen relative">
       <div className="container mx-auto relative z-10">
-        <div className="flex flex-col md:flex-row min-h-[calc(100vh-5rem)] mt-20 items-center">
+        <div className="flex flex-col md:flex-row min-h-[calc(100vh-5rem)] mt-20 mb-20 items-center">
           {/* Left side - Contact Form */}
           <div className="w-full md:w-1/2 p-8 md:p-16 flex items-center">
             <ContactForm />
