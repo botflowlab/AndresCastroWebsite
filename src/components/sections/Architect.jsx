49898d559b36a2e323769c265cb498a72a74795b
botflowlab <botflowlab@gmail.com>
2025-05-31 20:00:08 -0600

    Updated Architect.jsx

--- src/components/sections/Architect.jsx
+++ src/components/sections/Architect.jsx
@@ -10,7 +10,7 @@ function Architect() {
       <div className="absolute inset-0 w-full h-full">
         {/* Pattern Background */}
         <div 
-          className="absolute inset-0 w-full h-full opacity-50"
+          className="absolute inset-0 w-full h-full opacity-20"
           style={{
             backgroundImage: 'url(/images/concrete1.png)',
             backgroundRepeat: 'repeat',
