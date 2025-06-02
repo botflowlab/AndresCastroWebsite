3a9a2195998c02f4e60413af33fb89e1c3f4d8d9
botflowlab <botflowlab@gmail.com>
2025-06-02 14:20:51 -0600

    Updated About.jsx

--- src/pages/architect/About.jsx
+++ src/pages/architect/About.jsx
@@ -18,7 +18,7 @@ export default function About() {
           </div>
           
           {/* Right side - Content */}
-          <div className="w-full md:w-1/2 items-center">
+          <div className="w-full md:w-1/2">
             <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-light mb-8 tracking-wide whitespace-nowrap">
               MEET THE ARCHITECT
             </h2>
