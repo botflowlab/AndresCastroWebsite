58468f893131902020d97e224c68f1354936265e
botflowlab <botflowlab@gmail.com>
2025-06-02 14:20:17 -0600

    Updated About.jsx

--- src/pages/architect/About.jsx
+++ src/pages/architect/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
   const { t } = useTranslation();
   
   return (
-    <section className="py-20 px-4 relative">
+    <section className="py-20 px-4 relative overflow-hidden">
       <div className="max-w-screen-xl mx-auto">
         <div className="relative flex flex-col md:flex-row gap-12 md:gap-24 items-start md:items-stretch">
           {/* Left side - Image */}
