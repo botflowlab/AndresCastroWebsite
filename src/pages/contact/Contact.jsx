639ec45ea925c85d774df3fec6c0dd89cf00b680
botflowlab <botflowlab@gmail.com>
2025-06-05 22:46:19 -0600

    Updated Contact.jsx

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -41,20 +41,6 @@ export default function Contact() {
           </div>
         </div>
       </section>
-
-      {/* Map Section */}
-      <section className="h-[400px] relative overflow-hidden">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3929.853557856596!2d-84.0913878!3d9.935736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0e35cd6c7b373%3A0x6d0557089起点!2sSan%20José%2C%20Costa%20Rica!5e0!3m2!1sen!2s!4v1234567890"
-          width="100%"
-          height="100%"
-          style={{ border: 0 }}
-          allowFullScreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-          className="grayscale"
-        ></iframe>
-      </section>
     </div>
   );
 }
