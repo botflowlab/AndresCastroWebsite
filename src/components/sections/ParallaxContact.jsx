e7b953a4d26fb1bdd94eb67c073123f5a93b97a7
botflowlab <botflowlab@gmail.com>
2025-06-05 22:42:52 -0600

    Updated ParallaxContact.jsx

--- src/components/sections/ParallaxContact.jsx
+++ src/components/sections/ParallaxContact.jsx
@@ -20,7 +20,7 @@ function ParallaxContact() {
             ¿EN QUÉ TE PUEDO AYUDAR?
           </h2>
           <p className="text-xl md:text-2xl mb-16 font-cormorant font-light text-shadow">
-            Contáctanos.
+            Contácto.
           </p>
           <Link 
             to="/contacto" 
