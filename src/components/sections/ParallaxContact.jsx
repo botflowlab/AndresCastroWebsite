07b7acc1df7820dc8cc1d959245381ece6efecb8
botflowlab <botflowlab@gmail.com>
2025-06-05 20:32:09 -0600

    Updated ParallaxContact.jsx

--- src/components/sections/ParallaxContact.jsx
+++ src/components/sections/ParallaxContact.jsx
@@ -7,7 +7,7 @@ function ParallaxContact() {
       <div 
         className="absolute inset-0 bg-fixed bg-center bg-cover bg-no-repeat backdrop-blur-sm"
         style={{
-          backgroundImage: 'url(https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg)',
+          backgroundImage: 'url(/images/theArchitect/acCta.jpg)',
           backgroundAttachment: 'fixed'
         }}
       >
