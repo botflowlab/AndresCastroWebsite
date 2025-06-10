5ed14645a0adf9c1c1d4855a8feceb07b94f8a74
botflowlab <botflowlab@gmail.com>
2025-06-10 17:27:39 -0600

    Updated ArchitecturalDrawings.jsx

--- src/pages/projects/ArchitecturalDrawings.jsx
+++ src/pages/projects/ArchitecturalDrawings.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 export default function ArchitecturalDrawings({ project }) {
   const { t } = useTranslation();
