0652aa6847dd7191f01930c192c508e25b81a80a
botflowlab <botflowlab@gmail.com>
2025-06-10 17:09:03 -0600

    Updated Hero.jsx

--- src/pages/architect/Hero.jsx
+++ src/pages/architect/Hero.jsx
@@ -10,7 +10,8 @@ export default function Hero() {
       <div 
         className="absolute inset-0 bg-center bg-cover bg-no-repeat"
         style={{
-          backgroundImage: 'url(./images/theArchitect/hero1.jpg)'
+          backgroundImage: 'url(./images/theArchitect/ac6
+            .jpg)'
         }}
       >
         {/* Dark Overlay */}
